92ca32a7101d861b2ceca89d69a9387e0a4e0e82
a8espinoza <a8espinoza@ucsd.edu>
2024-12-18 18:09:04 -0800

    added multi outs, subfolders, seems to work

--- organizationScript.py
+++ organizationScript.py
@@ -29,10 +29,46 @@ listofPaths = []
 for path in listOfPathsInInput:
     listofPaths.append(inputDir + "/" + path)
 
-print(listofPaths)
+#print(listofPaths)
 
 
 for path in listofPaths:
+    #Move file to given Category Directory
     if(os.path.isfile(path)):
-        shutil.move(path, outputDirectories[0])
+        #All Homework
+        if(path.find("HW") != -1 or path.find("Homework") != -1):
+            if(path.find("20E") != -1):
+                class1Dir = outputDir1 + "/Math 20E"
+                if(not os.path.exists(class1Dir)):
+                    os.mkdir(class1Dir)
+                shutil.move(path, class1Dir)
+            elif(path.find("103A") != -1):
+                class2Dir = outputDir1 + "/Math 103A"
+                if(not os.path.exists(class2Dir)):
+                    os.mkdir(class2Dir)
+                shutil.move(path, class2Dir)
+            elif(path.find("154") != -1):
+                class3Dir = outputDir1 + "/Math 154"
+                if(not os.path.exists(class3Dir)):
+                    os.mkdir(class3Dir)
+                shutil.move(path, class3Dir)
+                '''
+            Uncomment this block for class4
+            elif(path.find("egclass") != -1):
+                class4Dir = outputDir1 + "egclass"
+                if(not os.path.exists(class4Dir)):
+                    os.mkdir(class4Dir)
+                shutil.move(path, class4Dir)
+                '''
+                
+            else:
+                shutil.move(path, outputDir1)
+                
+        #Important documents
+        elif(path.find("Important") != -1 or path.find("Alejandro") != -1):
+            shutil.move(path, outputDir2)
+        #Other documents
+        elif(path.find(".pdf") != -1):
+            shutil.move(path, outputDir3)
         
+
