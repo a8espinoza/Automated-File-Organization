9375e049775deaca9addac5819107b4d30c64d98
a8espinoza <a8espinoza@ucsd.edu>
2025-06-13 13:40:24 -0700

    setting up inputs, no subdirectories yet

--- organizationScript.py
+++ organizationScript.py
@@ -1,82 +1,62 @@
 import os, shutil
-'''
-#I need to figure out git first
-downloadsDirectory = "C:/Users/aleja/Downloads"
-testDirectory = "C:/Users/aleja/Desktop/FileOrganizer"
-destinationDirectory = "C:/Users/aleja/Desktop/FileOrganizer/moveHere"
-exFile = "C:/Users/aleja/Desktop/FileOrganizer/HW4.pdf"
-list = os.listdir(testDirectory) #list of files & directories in path
-print(list) #print list
-print(os.path.abspath(testDirectory)) #print given path
-print(testDirectory)
-boolean = os.path.exists(testDirectory)
-print(boolean)
-#a path can be a directory or a file
-#shutil.move(testDirectory + "/HW4_sol.pdf", destinationDirectory)
 
-Everything above is for reference and testing purposes
-'''
-#add all directories here (a path can be either a directory or directory)
-inputDir = "C:/Users/aleja/Desktop/FileOrganizer"
-outputDir1 = "C:/Users/aleja/Desktop/FileOrganizer/output1"
-outputDir2 = "C:/Users/aleja/Desktop/FileOrganizer/output2"
-outputDir3 = "C:/Users/aleja/Desktop/FileOrganizer/output3"
-outputDirectories = [outputDir1, outputDir2, outputDir3]
+#add main file to organize here
+inputDirString = "C:/Users/aleja/Desktop/FileOrganizer"
+inputDir = os.path.abspath(inputDirString)
 
+#add all names of directories you want to create
+outputDirectoryNames = ["output1", "output2", "output3"]
+numberOfOutputs = len(outputDirectoryNames)
 
-listOfPathsInInput = os.listdir(inputDir)
-listofPaths = []
-for path in listOfPathsInInput:
-    listofPaths.append(inputDir + "/" + path)
+#create all output directories
+outputDirectoryLocations = [
+    os.path.join(inputDir, name) for name in outputDirectoryNames
+]
 
-print(listOfPathsInInput)
-print(listofPaths)
+#outputDir1 = ["HW", "TextBook"]
+#outputDir1Subdir = ["Math 20E", "Math 103A", "Math 154"]
+#outputDir2 = ["Important", "Alejandro"]
+#outputDir2Subdir = []
+#outputDir3 = ["Misc", ".pdf", ".docx", ".txt"]
+#outputDir3Subdir = []
 
+#In the same order as outputDirectoryLocations, add the keywords that will be used to categorize the files.
+outputDirectyKeyWords = [
+    ["HW", "TextBook"],
+    ["Important", "Alejandro"],
+    ["Misc", ".pdf", ".docx", ".txt"],
+]
+#outputSubDirectories = [outputDir1Subdir, outputDir2Subdir, outputDir3Subdir]
 
-for path in listofPaths:
-    #Move file to given Category Directory
-    if(os.path.isfile(path)):
-        #All Homework
-        if(path.find("HW") != -1 or path.find("TextBook") != -1):
-            #create path if it does not exists
-            if(not os.path.exists(outputDir1)):
-                os.mkdir(outputDir1)
-            #sort through all possible courses
-            if(path.find("20E") != -1):
-                course1Dir = outputDir1 + "/Math 20E"
-                if(not os.path.exists(course1Dir)):
-                    os.mkdir(course1Dir)
-                shutil.move(path, course1Dir)
-            elif(path.find("103A") != -1):
-                course2Dir = outputDir1 + "/Math 103A"
-                if(not os.path.exists(course2Dir)):
-                    os.mkdir(course2Dir)
-                shutil.move(path, course2Dir)
-            elif(path.find("154") != -1):
-                course3Dir = outputDir1 + "/Math 154"
-                if(not os.path.exists(course3Dir)):
-                    os.mkdir(course3Dir) #
-                shutil.move(path, course3Dir)
-                '''  
-            Uncomment this block for course4
-            elif(path.find("egcourse") != -1):
-                course4Dir = outputDir1 + "egcourse"
-                if(not os.path.exists(course4Dir)):
-                    os.mkdir(course4Dir)
-                shutil.move(path, course4Dir)
-                   '''
-            else:
-                shutil.move(path, outputDir1)
+while True:
+    listOfPathsInInput = os.listdir(inputDir)  # Get the latest list of files
+    moved_files = 0  # Track if we moved anything
+
+    for file in listOfPathsInInput:
+        path = os.path.join(inputDir, file)  # Get the full path of the file
+        print(path)
+        #Move file to given Category Directory
+        if(os.path.isfile(path)):
+            #loop through all directories
+            for i in range(len(outputDirectories)):
+                #check if directory exists
+                if(not os.path.exists(outputDirectoryLocations[i])):
+                    os.mkdir(outputDirectoryLocations[i])
                 
-        #Important documents
-        elif(path.find("Important") != -1 or path.find("Alejandro") != -1):
-            if(not os.path.exists(outputDir2)):
-                os.mkdir(outputDir2)
-            shutil.move(path, outputDir2)
-        #Other documents
-        elif(path.find(".pdf") != -1):
-            if(not os.path.exists(outputDir3)):
-                os.mkdir(outputDir3)
-            shutil.move(path, outputDir3)
-        
+                for j in outputDirectories[i]:
+                    #check if given path contains given directory name
+                    if j in file:  # Check if filename contains a keyword
+                        dest_path = os.path.join(outputDirectoryLocations[i], file)
+                        #create path if it does not exists
+                        shutil.move(path, dest_path)
+                        moved_files += 1
+                        break
+
+
+    if moved_files == 0:
+        break  # Exit loop if no files were moved
+
+                                
+
+            
 
